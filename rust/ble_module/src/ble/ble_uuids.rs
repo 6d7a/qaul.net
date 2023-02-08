675a03ee0a8c65755a19a347c51ffe81e5166ac7
6d7a <kwestphal2901@gmail.com>
2023-02-08 18:14:36 +0100

    feat(ble): add BLE UUIDs

---
+++ rust/ble_module/src/ble/ble_uuids.rs
@@ -0,0 +1,5 @@
+pub const SERVICE_UUID: &str = "99e91399-80ed-4943-9bcb-39c532a76023";
+pub const MSG_SERVICE_UUID: &str = "99e91400-80ed-4943-9bcb-39c532a76023";
+pub const READ_CHAR: &str = "99e91401-80ed-4943-9bcb-39c532a76023";
+pub const MSG_CHAR: &str = "99e91402-80ed-4943-9bcb-39c532a76023";
+pub const GD_CHAR: &str = "99e91403-80ed-4943-9bcb-39c532a76023";
