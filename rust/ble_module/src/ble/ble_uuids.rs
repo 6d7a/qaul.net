d352a724bfc882dac79c1a1fca0aa2fd5b1ca4bf
6d7a <kwestphal2901@gmail.com>
2023-02-09 18:20:53 +0100

    refactor(uuid): rename to main UUID

--- rust/ble_module/src/ble/ble_uuids.rs
+++ rust/ble_module/src/ble/ble_uuids.rs
@@ -1,4 +1,4 @@
-pub const SERVICE_UUID: &str = "99e91399-80ed-4943-9bcb-39c532a76023";
+pub const MAIN_SERVICE_UUID: &str = "99e91399-80ed-4943-9bcb-39c532a76023";
 pub const MSG_SERVICE_UUID: &str = "99e91400-80ed-4943-9bcb-39c532a76023";
 pub const READ_CHAR: &str = "99e91401-80ed-4943-9bcb-39c532a76023";
 pub const MSG_CHAR: &str = "99e91402-80ed-4943-9bcb-39c532a76023";
