898a4880bdd386897d74f933456a4f06a008ed28
6d7a <kwestphal2901@gmail.com>
2023-02-16 09:17:49 +0100

    refactor(ble): qaul ble trait to fit ble sys msgs

--- rust/ble_module/src/ble/ble_manager.rs
+++ rust/ble_module/src/ble/ble_manager.rs
@@ -11,12 +11,16 @@ pub struct QaulBleAppEventRx {
 }
 
 #[async_trait]
-pub trait QaulBleManager {
-    async fn advertise(&mut self, advert_mode: Option<i16>) -> Result<(), Box<dyn Error>>;
+pub trait QaulBleConnect {
+    async fn start_advertise_and_listen(
+        &mut self,
+        advert_mode: Option<i16>,
+    ) -> Result<(), Box<dyn Error>>;
     async fn start_ble_app(&mut self, qaul_id: &Bytes)
         -> Result<QaulBleAppEventRx, Box<dyn Error>>;
     async fn scan(
         &mut self,
     ) -> Result<Pin<Box<dyn futures::Stream<Item = Address>>>, Box<dyn Error>>;
+    async fn send_directly(&mut self) -> Result<QaulBleAppEventRx, Box<dyn Error>>;
     fn close(&self);
 }
