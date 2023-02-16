8e7476b16ae8f8416505b0a3d171b2e06c17d698
6d7a <kwestphal2901@gmail.com>
2023-02-16 09:31:44 +0100

    feat(loop): forward sys msgs to ble impl

--- rust/ble_module/src/ble_connector.rs
+++ rust/ble_module/src/ble_connector.rs
@@ -1,12 +1,14 @@
+use std::error::Error;
+
 use crate::{
-    ble::ble_manager::QaulBleConnect,
+    ble::{self, ble_manager::QaulBleConnect},
     rpc::{proto_sys::ble::Message::*, SysRpcReceiver},
 };
 
 pub async fn run_ble_connector_loop(
     mut rpc_receiver: Box<dyn SysRpcReceiver>,
     mut ble_service: Box<dyn QaulBleConnect>,
-) {
+) -> Result<(), Box<dyn Error> {
     loop {
         let evt = rpc_receiver.recv().await;
         match evt {
@@ -20,13 +22,14 @@ pub async fn run_ble_connector_loop(
                     continue;
                 }
                 match msg.message.unwrap() {
-                    InfoRequest(req) => todo!(),
-                    StartRequest(req) => todo!(),
-                    StopRequest(req) => todo!(),
-                    DirectSend(req) => todo!(),
+                    InfoRequest(req) => ble_service.get_device_info().await?,
+                    StartRequest(req) => ble_service.advertise_scan_listen(None).await?,
+                    StopRequest(req) => ble_service.close(),
+                    DirectSend(req) => ble_service.send_directly().await?,
                     _ => (),
                 }
             }
         }
     }
+    Ok(())
 }
